74b85bf25a9026f4f4b76012526baf9a07a6db28
Nicole <nicole_a_alejandro@live.com>
2018-10-07 20:33:51 -0400

    Created template directory using jinja2, added html form

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__),
+    'templates')
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(template_dir))    
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,6 +14,7 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    
+    template = jinja_env.get_template('form.html')
+    return template.render()
 
 app.run()   
